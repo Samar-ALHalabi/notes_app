9c6c7d9b23ec4981d72759538961f8dc16e2d51f
Samar-ALHalabi <samaralhalabi1978@gmail.com>
2024-03-26 07:02:08 +0300

    customItem

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Notes_App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(home: Notes_App());
-  }
-}
-
+// ignore: camel_case_types
 class Notes_App extends StatelessWidget {
   const Notes_App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(backgroundColor: Colors.amberAccent),
-       
+    return MaterialApp(theme: ThemeData.light(), home: SafeArea());
   }
 }
